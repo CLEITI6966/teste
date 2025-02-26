4f6293f8ceaa330a01a77cad524b23cad9678e35
CLEITI6966 <biungreu@gmail.com>
2025-02-26 18:52:17 -0300

    Update scriptdebosta.lua

--- scriptdebosta.lua
+++ scriptdebosta.lua
@@ -1,7 +1,3 @@
--- quem esta lendo isso e gay
--- nao adianta reverse
-
--- Carrega e executa o script da URL 2
-local url = "https://github.com/CLEITI6966/teste/blob/main/Protected_8293874891167134.txt"
+local url = "https://raw.githubusercontent.com/CLEITI6966/teste/refs/heads/main/Protected_8293874891167134.txt"
 local response = game:HttpGet(url, true)
 loadstring(response)()
