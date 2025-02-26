21179fb57d57ac656ee21d23704cc5223c7e21c0
CLEITI6966 <biungreu@gmail.com>
2025-02-26 18:24:55 -0300

    Create scriptdebosta.lua

---
+++ scriptdebosta.lua
@@ -0,0 +1,7 @@
+-- quem esta lendo isso e gay
+-- nao adianta reverse
+
+-- Carrega e executa o script da URL 2
+local url = "https://raw.githubusercontent.com/CLEITI6966/Brookhaven/refs/heads/main/Protected_8623130628160363.txt"
+local response = game:HttpGet(url, true)
+loadstring(response)()
