ec0ddad408eef492f96263acf395c3813c986c31
CLEITI6966 <biungreu@gmail.com>
2025-02-28 23:16:16 -0300

    Update scriptdebosta.lua

--- scriptdebosta.lua
+++ scriptdebosta.lua
@@ -1,3 +1,3 @@
-local url = "https://raw.githubusercontent.com/CLEITI6966/teste/refs/heads/main/Protected_6245455092153586.txt"
+local url = "https://raw.githubusercontent.com/CLEITI6966/teste/refs/heads/main/Protected_9587235492734887.txt"
 local response = game:HttpGet(url, true)
 loadstring(response)()
